9a02c055bad607896f1d6cdc897da1ee70288ae2
magdakok <61601293+magdakok@users.noreply.github.com>
2020-05-03 13:27:42 +0100

    add function that creates deck of 56 cards

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-function myDeck() {
+function makeDeck() {
     const suits = ['heart', 'diamond', 'spade', 'club'];
     const values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']; 
-   
+    const deck = [];
+
+    for (let value of values) {
+        for (let suit of suits) {
+            deck.push({value,suit});
+        }
+    }
+    return deck;
 }
+
+//deck of 56 cards
+const myDeck = makeDeck(); 
