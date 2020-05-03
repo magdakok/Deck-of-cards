03614263ce2ff5e7c4a864ff09709fc28429d3ed
magdakok <61601293+magdakok@users.noreply.github.com>
2020-05-03 14:42:21 +0100

    create object with initializeDeck method

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
-function makeDeck() {
-    const suits = ['heart', 'diamond', 'spade', 'club'];
-    const values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']; 
-    const deck = [];
-
-    for (let value of values) {
-        for (let suit of suits) {
-            deck.push({value,suit});
+const myDeck = {
+    deck: [],
+    suits: ['heart', 'diamond', 'spade', 'club'],
+    values: ['2','3','4','5','6','7','8','9','10','J','Q','K','A'],
+    initializeDeck() {
+        const deck = []; 
+        for (let value of this.values) {
+            for (let suit of this.suits) {
+                deck.push({value,suit});
+            }
         }
+        this.deck = deck;
     }
-    return deck;
 }
-
-//deck of 56 cards
-const myDeck = makeDeck(); 
