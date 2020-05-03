bc631f991366c2e9de2693d54fae97c36c4145d8
magdakok <61601293+magdakok@users.noreply.github.com>
2020-05-03 16:34:15 +0100

    add drawnCards array and random pick mechanism

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const myDeck = {
     deck: [],
+    drawnCards: [],
     suits: ['heart', 'diamond', 'spade', 'club'],
     values: ['2','3','4','5','6','7','8','9','10','J','Q','K','A'],
     initializeDeck() {
@@ -10,5 +11,21 @@ const myDeck = {
             }
         }
         this.deck = deck;
+    },
+    drawCard() {
+        if (this.deck.length) {
+            //pick random index
+            const idx = Math.floor(Math.random()*this.deck.length);
+            console.log(idx);
+
+            //remove random card from the deck
+            const card = this.deck.splice(idx,1);
+
+            //add picked card to drawnCards array
+            this.drawnCards.push(...card);
+        } else {
+            console.log('No cards in the deck');
+        }
+        
     }
 }
